3b5a0c7a7f3ac64e38158dd8744a54d5e35dfe66
Xinyi-Lai <xinyi.17@intl.zju.edu.cn>
2024-01-09 15:30:34 +0800

    download, loading, card alignment

--- dashboard/ccpred-dashboard/app.py
+++ dashboard/ccpred-dashboard/app.py
@@ -14,6 +14,7 @@ app.layout = dbc.Container(
                 dbc.NavItem(dbc.NavLink('Home', href='/')),
                 dbc.NavItem(dbc.NavLink('Correlation', href='correlation')),
                 dbc.NavItem(dbc.NavLink('Prediction', href='prediction')),
+                dbc.NavItem(dbc.NavLink('Ref', href='ref')),
             ],
             brand='Carbon Price Forecasting',
             brand_href='/',
--- dashboard/ccpred-dashboard/pages/correlation.py
+++ dashboard/ccpred-dashboard/pages/correlation.py
@@ -1,9 +1,9 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import dcc, html, Input, Output, dash_table, callback
 import dash_bootstrap_components as dbc
 
-import plotly.graph_objects as go
 import pandas as pd
+import plotly.graph_objects as go
 from sqlalchemy import create_engine
 
 
@@ -20,56 +20,112 @@ Xvar_list = ['EU-CC', 'WTI-Oil', 'Brent-Oil', 'Zhengzhou-Coal', 'Dalian-Coal', '
 
 layout = dbc.Container(
     [
+        # TODO move to home page, data description
+        ### download data
+        dbc.ButtonGroup([
+            dbc.Button('Download CSV', id='down-btn-csv'),
+            dcc.Download(id='down-df-csv'),
+            dbc.Button('Download Excel', id='down-btn-excel'), 
+            dcc.Download(id='down-df-excel'),
+            dbc.Button('Download db', href='/assets/CarbonPrice.db', download='DownCarbonPrice.db', external_link=True),
+            dbc.Button('Download Paper', id='down-btn-paper', disabled=True), 
+        ], class_name='mt-3 gap-2'),
+
+        ### show Xvars and Yvars
         dbc.Row([
             dbc.Col([
                 dbc.Card([
-                    dbc.CardHeader('Carbon Prices', className='text-center'),
+                    dbc.CardHeader('Carbon Prices', class_name='text-center'),
                     dbc.CardBody([
                         dcc.Dropdown(id='select-market', multi=True, options=city_list, value=['Guangzhou', 'Hubei', 'Shanghai'], placeholder='Select market...'),
-                        dcc.Graph(id='graph-Cprice', className='pb-4'),
+                        dcc.Graph(id='graph-Cprice'),
                     ]),
-                ], className='mt-4'),
-            ], width=12, lg=6, className='pt-4'),
-
+                ]),
+            ], class_name='mt-4 mb-2'),
             dbc.Col([
                 dbc.Card([
-                    dbc.CardHeader('Exogenous Variables', className='text-center'),
+                    dbc.CardHeader('Exogenous Variables', class_name='text-center'),
                     dbc.CardBody([
                         dcc.Dropdown(id='select-Xvar', multi=True, options=Xvar_list, value=['Brent-Oil', 'Rtd-Coal', 'USD-CNY'], placeholder='Select X variable...'),
-                    dcc.Graph(id='graph-Xvar', className='pb-4'),
+                        dcc.Graph(id='graph-Xvar'),
                     ]),
-                ], className='mt-4'),
-            ], width=12, lg=6, className='pt-4'),
-            
-        ]),
+                ]),
+            ], class_name='mt-4 mb-2'),
+        ], class_name='row-cols-1 row-cols-lg-2'),
 
-        dbc.Row([
-            dbc.Col(width=4, lg=2, className='pt-4', children=[
-                html.H6('Select X vars:'),
-                dcc.Checklist(id='select-XvarCorr', options=Xvar_list, value=Xvar_list),
+        dash_table.DataTable(
+            id='table-data',
+            columns=[{"name": i, "id": i} for i in df_chn.columns],
+            data=df_chn.to_dict('records'),
+            style_cell={'textAlign': 'center', 'minWidth': '100px', 'width': '100px', 'maxWidth': '100px'},
+            style_header={'fontWeight': 'bold'},
+            style_data_conditional=[{'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(248, 248, 248)'}],
+            style_table={'overflowX': 'auto'},
+            page_size=20,
+            fixed_rows={'headers': True},
+            style_as_list_view=True,
+            style_cell_conditional=[{'if': {'column_id': 'Date'}, 'width': '150px'}],
+            style_data={'whiteSpace': 'normal', 'height': 'auto'},
+        ),
+
+        ### show correlation
+        dbc.Card([
+            dbc.CardHeader('Correlation Analysis', class_name='text-center'),
+            dbc.CardBody([
+                dbc.Row([
+                    # correlation options
+                    dbc.Col([
+                        dbc.Row([
+                            dbc.Col([html.H6('Select X vars:')]),
+                            dbc.Col([html.H6('Select Y vars:')]),
+                            dbc.Col([html.H6('Select Corr method:')]),
+                            dbc.Col([dcc.Checklist(id='select-XvarCorr', options=Xvar_list, value=Xvar_list)]),
+                            dbc.Col([dcc.Checklist(id='select-YvarCorr', options=city_list, value=['Guangzhou', 'Hubei', 'Shanghai'])]),
+                            dbc.Col([dcc.RadioItems(id='select-CorrType', options=['pearson', 'kendall', 'spearman'], value='pearson')]),
+                        ], class_name='row-cols-3') # 3 cols in a row and align automatically
+                    ], class_name='mt-4'),
+                    # correlation graph
+                    dbc.Col([
+                        dcc.Loading(id='loading-VarCorr', type='graph', children=[
+                            dcc.Graph(id='graph-VarCorr')
+                        ]),  
+                    ])
+                ], class_name='row-cols-1 row-cols-lg-2')
+            ], class_name='ms-3'),
+        ], class_name='mt-3'),
+
+        ### other correlation analysis
+        dbc.Card([
+            dbc.CardHeader('Other Correlation Analysis', class_name='text-center'),
+            dbc.CardBody([
+                dbc.Row( [ dbc.Col([html.H6('Analysis')]) ]*20, class_name='row-cols-4 gap-3') # 3 cols in a row and align automatically
             ]),
-            dbc.Col(width=4, lg=2, className='pt-4', children=[
-                html.H6('Select Y vars:'),
-                dcc.Checklist(id='select-YvarCorr', options=city_list, value=['Guangzhou', 'Hubei', 'Shanghai']),
-            ]),
-            dbc.Col(width=4, lg=2, className='pt-4', children=[
-                html.H6('Select Corr method:'),
-                dcc.RadioItems(id='select-CorrType', options=['pearson', 'kendall', 'spearman'], value='pearson'),
-            ]),
-            
-            # dcc.Loading(id='loading-VarCorr', type='circle', children=[
-                
-            # ]),
-            dbc.Col(width=12, lg=6, className='pt-4', children=[
-                    dcc.Graph(id='graph-VarCorr', className='pb-4')
-                ])
-            
-        ]),
+        ], class_name='mt-5'),
 
     ], fluid=True,
 )
 
 
+# FUTURE: upload data
+### download data
+@callback(
+    Output('down-df-csv', 'data'),
+    Input('down-btn-csv', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def down_df_csv(n_clicks):
+    return dcc.send_data_frame(df_chn.to_csv, "mydf.csv")
+
+@callback(
+    Output('down-df-excel', 'data'),
+    Input('down-btn-excel', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def down_df_excel(n_clicks):
+    return dcc.send_data_frame(df_chn.to_excel, "mydf.xlsx", sheet_name="Sheet_name_1")
+
+
+### show Xvars and Yvars
 @callback(
     Output('graph-Cprice', 'figure'),
     Input('select-market', 'value'),
@@ -92,7 +148,7 @@ def graph_Cprice(selected_markets):
         showlegend=True,
         legend=dict(x=0.01, y=0.99),
         height=400,
-        margin=dict(l=40, r=10, t=60, b=55),
+        margin=dict(l=40, r=10, t=30, b=55),
         yaxis=dict(title='Price', tickprefix='￥'),
         xaxis=dict(title='Date'),
     )
@@ -118,12 +174,13 @@ def graph_Xvar(selected_Xvars):
         showlegend=True,
         legend=dict(x=0.01, y=0.99),
         height=400,
-        margin=dict(l=40, r=10, t=60, b=55),
+        margin=dict(l=40, r=10, t=30, b=55),
         yaxis=dict(title='Normalized Value'),
         xaxis=dict(title='Date'),
     )
     return fig
 
+### show Correlation
 @callback(
     Output('graph-VarCorr', 'figure'),
     Input('select-XvarCorr', 'value'),
@@ -138,13 +195,12 @@ def graph_VarCorr(selected_XvarCorr, selected_YvarCorr, selected_CorrType):
         y=df_corr.columns, 
         zmin=-1, zmax=1, colorscale='RdBu_r'
     )) 
-    
     fig.update_layout(
         title='Correlation Heatmap',
         template='none',
         showlegend=True,
         legend=dict(x=0.01, y=0.99),
-        height=600,
+        height=500,
         margin=dict(l=80, r=10, t=60, b=70),
     )
     return fig
